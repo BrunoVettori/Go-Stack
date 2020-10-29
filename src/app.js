35cb7408a99a8f90340d90a34b56f2fc7d611596
Bruno Vettori <koikke21@gmail.com>
2020-10-28 23:02:16 -0300

    a

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,15 +11,35 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return(response.status(200).json(repositories))
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const {title, url, techs } = request.body
+
+  const likes = 0
+
+  repositories.push({id: uuid(), title, url, techs, likes})
+
+  console.log(repositories)
+
+  return(response.status(200).json(repositories[repositories.length -1 ]))
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const {id} = request.params
+
+  const repositori = repositories.find(repositori => repositori.id == id)
+
+  console.log('Repo found: ', repositori)
+
+  return(response.status(200).json(repositori))
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
